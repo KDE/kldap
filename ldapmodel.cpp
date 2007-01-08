f73fd562e1f59506552c111a4afea21fb52f5a71
Allen Winter <winter@kde.org>
2007-01-08 00:47:23 +0000

    indenting and coding style fixes.
    svn path=/trunk/KDE/kdepimlibs/; revision=621007

--- ldapmodel.cpp
+++ ldapmodel.cpp
@@ -1,21 +1,21 @@
 /*
-    This file is part of libkldap.
-    Copyright (c) 2006 Sean Harmer <sh@theharmers.co.uk>
+  This file is part of libkldap.
+  Copyright (c) 2006 Sean Harmer <sh@theharmers.co.uk>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General  Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General  Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #include <kdebug.h>
@@ -27,184 +27,175 @@
 
 using namespace KLDAP;
 
-LdapModel::LdapModel( QObject* parent )
-    : QAbstractItemModel( parent ),
-      m_d( new LdapModelPrivate( this ) )
+LdapModel::LdapModel( QObject *parent )
+  : QAbstractItemModel( parent ),
+    m_d( new LdapModelPrivate( this ) )
 {
-    m_d->createConnections();
+  m_d->createConnections();
 }
 
-
-LdapModel::LdapModel( LdapConnection& connection, QObject* parent )
-    : QAbstractItemModel( parent ),
-      m_d( new LdapModelPrivate( this, connection ) )
+LdapModel::LdapModel( LdapConnection &connection, QObject *parent )
+  : QAbstractItemModel( parent ),
+    m_d( new LdapModelPrivate( this, connection ) )
 {
-    m_d->createConnections();
+  m_d->createConnections();
 
-    // Populate items from the root object to that representing the baseDN
-    m_d->populateRootToBaseDN();
+  // Populate items from the root object to that representing the baseDN
+  m_d->populateRootToBaseDN();
 }
-
 
 LdapModel::~LdapModel()
 {
-    delete m_d;
+  delete m_d;
 }
 
-
-void LdapModel::setConnection( LdapConnection& connection )
+void LdapModel::setConnection( LdapConnection &connection )
 {
-    m_d->setConnection( connection );
+  m_d->setConnection( connection );
 
-    // Refresh the model
-    m_d->recreateRootItem();
+  // Refresh the model
+  m_d->recreateRootItem();
 
-    // Populate the root object by searching the baseDN
-    m_d->populateRootToBaseDN();
+  // Populate the root object by searching the baseDN
+  m_d->populateRootToBaseDN();
 }
 
-
-QModelIndex LdapModel::index( int row, int col, const QModelIndex& parent ) const
+QModelIndex LdapModel::index( int row, int col, const QModelIndex &parent ) const
 {
-    LdapModelTreeItem* parentItem;
-    if ( !parent.isValid() )
-        parentItem = m_d->rootItem();
-    else
-        parentItem = static_cast<LdapModelTreeItem*>( parent.internalPointer() );
+  LdapModelTreeItem *parentItem;
+  if ( !parent.isValid() ) {
+    parentItem = m_d->rootItem();
+  } else {
+    parentItem = static_cast<LdapModelTreeItem*>( parent.internalPointer() );
+  }
 
-    LdapModelTreeItem* childItem = parentItem->child( row );
-    if ( childItem )
-        return createIndex( row, col, childItem );
-    else
-        return QModelIndex();
+  LdapModelTreeItem *childItem = parentItem->child( row );
+  if ( childItem ) {
+    return createIndex( row, col, childItem );
+  } else {
+    return QModelIndex();
+  }
 }
 
-
-QModelIndex LdapModel::parent( const QModelIndex& child ) const
+QModelIndex LdapModel::parent( const QModelIndex &child ) const
 {
-    if ( !child.isValid() )
-        return QModelIndex();
+  if ( !child.isValid() ) {
+    return QModelIndex();
+  }
 
-    LdapModelTreeItem* childItem = static_cast<LdapModelTreeItem*>( child.internalPointer() );
-    LdapModelTreeItem* parentItem = childItem->parent();
+  LdapModelTreeItem *childItem = static_cast<LdapModelTreeItem*>( child.internalPointer() );
+  LdapModelTreeItem *parentItem = childItem->parent();
 
-    if ( parentItem == m_d->rootItem() )
-        return QModelIndex();
+  if ( parentItem == m_d->rootItem() ) {
+    return QModelIndex();
+  }
 
-    return createIndex( parentItem->row(), 0, parentItem );
+  return createIndex( parentItem->row(), 0, parentItem );
 }
 
-
-QVariant LdapModel::data( const QModelIndex& index, int role ) const
+QVariant LdapModel::data( const QModelIndex &index, int role ) const
 {
-    if ( !index.isValid() )
-        return QVariant();
-
-    LdapModelTreeItem* item = static_cast<LdapModelTreeItem*>( index.internalPointer() );
-
-    if ( role == Qt::DisplayRole )
-    {
-        kDebug() << "***** LdapModel::data(): rdn = " << item->data().dn().rdnString() << endl;
-        return item->data().dn().rdnString();
-    }
-    else if ( role == Qt::ToolTipRole )
-    {
-        /** \TODO Make the tooltips look nicer - perhaps themeable? */
-        kDebug() << "***** LdapModel::data(): Object = " << item->data().toString() << endl;
-        return item->data().toString();
-    }
-
-    /** \TODO Include support for nice decorative icons dependent upon the objectClass + other role data. */
-
+  if ( !index.isValid() ) {
     return QVariant();
-}
+  }
 
+  LdapModelTreeItem *item = static_cast<LdapModelTreeItem*>( index.internalPointer() );
+
+  if ( role == Qt::DisplayRole ) {
+    kDebug() << "***** LdapModel::data(): rdn = " << item->data().dn().rdnString() << endl;
+    return item->data().dn().rdnString();
+  } else if ( role == Qt::ToolTipRole ) {
+    /** \TODO Make the tooltips look nicer - perhaps themeable? */
+    kDebug() << "***** LdapModel::data(): Object = " << item->data().toString() << endl;
+    return item->data().toString();
+  }
+
+  /** \TODO Include support for nice decorative icons dependent upon the objectClass + other role data. */
+
+  return QVariant();
+}
 
 QVariant LdapModel::headerData( int /*section*/, Qt::Orientation orientation, int role ) const
 {
-    if ( orientation == Qt::Horizontal && role == Qt::DisplayRole )
-    {
-        /** @TODO Hmm, what to do here? Override in proxymodels perhaps as could be "DN" or "Attribute". */
-        return QString( "Distinguished Name" );
-    }
+  if ( orientation == Qt::Horizontal && role == Qt::DisplayRole ) {
+    /** @TODO Hmm, what to do here? Override in proxymodels perhaps as could be "DN" or "Attribute". */
+    return QString( "Distinguished Name" );
+  }
 
-    return QVariant();
+  return QVariant();
 }
 
-
-Qt::ItemFlags LdapModel::flags( const QModelIndex& index ) const
+Qt::ItemFlags LdapModel::flags( const QModelIndex &index ) const
 {
-    /** \TODO Read-only for now, make read-write upon request */
-    if ( !index.isValid() )
-        return Qt::ItemIsEnabled;
+  /** \TODO Read-only for now, make read-write upon request */
+  if ( !index.isValid() ) {
+    return Qt::ItemIsEnabled;
+  }
 
-    return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+  return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
 }
 
-
-int LdapModel::columnCount( const QModelIndex& parent ) const
+int LdapModel::columnCount( const QModelIndex &parent ) const
 {
-    LdapModelTreeItem* parentItem = parent.isValid()
+  LdapModelTreeItem *parentItem = parent.isValid()
                                   ? static_cast<LdapModelTreeItem*>( parent.internalPointer() )
                                   : m_d->rootItem();
-    return parentItem->columnCount();
+  return parentItem->columnCount();
 }
 
-
-int LdapModel::rowCount( const QModelIndex& parent ) const
+int LdapModel::rowCount( const QModelIndex &parent ) const
 {
-    kDebug() << "LdapModel::rowCount" << endl;
-    if ( parent.column() > 0 )
-        return 0;
+  kDebug() << "LdapModel::rowCount" << endl;
+  if ( parent.column() > 0 ) {
+    return 0;
+  }
 
-    const LdapModelTreeItem* item = parent.isValid()
+  const LdapModelTreeItem *item = parent.isValid()
                                   ? static_cast<const LdapModelTreeItem*>( parent.internalPointer() )
                                   : m_d->rootItem();
-    kDebug() << "Parent (" << item->ldapObject().dn().toString() << ") has " << item->childCount() << " children" << endl;
-    return item->childCount();
+  kDebug() << "Parent (" << item->ldapObject().dn().toString() << ") has " << item->childCount() << " children" << endl;
+  return item->childCount();
 }
 
-
-bool LdapModel::hasChildren( const QModelIndex& parent ) const
+bool LdapModel::hasChildren( const QModelIndex &parent ) const
 {
-    // We always return true. This means that the branch expansion symbol will
-    // always be drawn. However, once the user clicks on it, rowCount() will
-    // get called and the view will not draw the expander if the item has no
-    // children.
-    const LdapModelTreeItem* item = parent.isValid()
+  // We always return true. This means that the branch expansion symbol will
+  // always be drawn. However, once the user clicks on it, rowCount() will
+  // get called and the view will not draw the expander if the item has no
+  // children.
+  const LdapModelTreeItem *item = parent.isValid()
                                   ? static_cast<const LdapModelTreeItem*>( parent.internalPointer() )
                                   : m_d->rootItem();
-    if ( !parent.isValid() || item->isPopulated() )
-        return ( item->childCount() > 0 );
-    return true;
+  if ( !parent.isValid() || item->isPopulated() ) {
+    return item->childCount() > 0;
+  }
+  return true;
 }
 
-
-bool LdapModel::canFetchMore( const QModelIndex& parent ) const
+bool LdapModel::canFetchMore( const QModelIndex &parent ) const
 {
-    const LdapModelTreeItem* item = parent.isValid()
+  const LdapModelTreeItem *item = parent.isValid()
                                   ? static_cast<const LdapModelTreeItem*>( parent.internalPointer() )
                                   : m_d->rootItem();
-    kDebug() << "LdapModel::canFetchMore() : " << !item->isPopulated() << endl;
-    return !item->isPopulated();
+  kDebug() << "LdapModel::canFetchMore() : " << !item->isPopulated() << endl;
+  return !item->isPopulated();
 }
 
-
-void LdapModel::fetchMore( const QModelIndex& parent )
+void LdapModel::fetchMore( const QModelIndex &parent )
 {
-    /** \TODO This should be altered to search for all attributes we can filter out those not required with a proxy model */
-    kDebug() << "LdapModel::fetchMore()" << endl;
+  /** \TODO This should be altered to search for all attributes we can filter out those not required with a proxy model */
+  kDebug() << "LdapModel::fetchMore()" << endl;
 
-    LdapModelTreeItem* parentItem = parent.isValid()
+  LdapModelTreeItem *parentItem = parent.isValid()
                                   ? static_cast<LdapModelTreeItem*>( parent.internalPointer() )
                                   : m_d->rootItem();
 
-    // Search for the immediate children of parentItem.
-    m_d->searchResults().clear();
-    m_d->setSearchType( LdapModelPrivate::ChildObjects, parentItem );
-    m_d->search( parentItem->data().dn(), LdapUrl::One, QString(), QStringList() << "dn" << "objectClass" );
-    parentItem->setPopulated( true );
+  // Search for the immediate children of parentItem.
+  m_d->searchResults().clear();
+  m_d->setSearchType( LdapModelPrivate::ChildObjects, parentItem );
+  m_d->search( parentItem->data().dn(), LdapUrl::One, QString(),
+               QStringList() << "dn" << "objectClass" );
+  parentItem->setPopulated( true );
 }
-
 
 #include "ldapmodel.moc"
--- ldapmodel.h
+++ ldapmodel.h
@@ -1,21 +1,21 @@
 /*
-    This file is part of libkldap.
-    Copyright (c) 2006 Sean Harmer <sh@theharmers.co.uk>
+  This file is part of libkldap.
+  Copyright (c) 2006 Sean Harmer <sh@theharmers.co.uk>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General  Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General  Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #ifndef KLDAP_LDAPMODEL_H
@@ -34,31 +34,31 @@ class LdapSearch;
 
 class KLDAP_EXPORT LdapModel : public QAbstractItemModel
 {
-    Q_OBJECT
-    public:
-        explicit LdapModel( QObject* parent = 0 );
-        explicit LdapModel( LdapConnection& connection, QObject* parent = 0 );
-        virtual ~LdapModel();
+  Q_OBJECT
+  public:
+    explicit LdapModel( QObject *parent = 0 );
+    explicit LdapModel( LdapConnection &connection, QObject *parent = 0 );
+    virtual ~LdapModel();
 
-        void setConnection( LdapConnection& connection );
+    void setConnection( LdapConnection &connection );
 
-        virtual QModelIndex index( int row, int col, const QModelIndex& parent ) const;
-        virtual QModelIndex parent( const QModelIndex& child ) const;
-        virtual QVariant data( const QModelIndex& index, int role ) const;
-        virtual QVariant headerData( int section, Qt::Orientation orientation, int role ) const;
-        virtual Qt::ItemFlags flags( const QModelIndex& index ) const;
-        virtual int columnCount( const QModelIndex& parent ) const;
-        virtual int rowCount( const QModelIndex& parent ) const;
-        virtual bool hasChildren( const QModelIndex& parent ) const;
-        virtual bool canFetchMore( const QModelIndex& parent ) const;
-        virtual void fetchMore( const QModelIndex& parent );
+    virtual QModelIndex index( int row, int col, const QModelIndex &parent ) const;
+    virtual QModelIndex parent( const QModelIndex &child ) const;
+    virtual QVariant data( const QModelIndex &index, int role ) const;
+    virtual QVariant headerData( int section, Qt::Orientation orientation, int role ) const;
+    virtual Qt::ItemFlags flags( const QModelIndex &index ) const;
+    virtual int columnCount( const QModelIndex &parent ) const;
+    virtual int rowCount( const QModelIndex &parent ) const;
+    virtual bool hasChildren( const QModelIndex &parent ) const;
+    virtual bool canFetchMore( const QModelIndex &parent ) const;
+    virtual void fetchMore( const QModelIndex &parent );
 
-    private:
-        class LdapModelPrivate;
-        LdapModelPrivate* const m_d;
+  private:
+    class LdapModelPrivate;
+    LdapModelPrivate *const m_d;
 
-        Q_PRIVATE_SLOT( m_d, void gotSearchResult( LdapSearch* ) )
-        Q_PRIVATE_SLOT( m_d, void gotSearchData( LdapSearch*, const LdapObject& ) )
+    Q_PRIVATE_SLOT( m_d, void gotSearchResult( LdapSearch* ) )
+    Q_PRIVATE_SLOT( m_d, void gotSearchData( LdapSearch*, const LdapObject& ) )
 };
 
 }
