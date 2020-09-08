043440d7f2d1a73c5d1fb05579c8425ec2ba163b
Christophe Giboudeaux <christophe@krop.fr>
2020-09-08 22:32:31 +0200

    Relicense LGPL-2.0-only files to LGPL-2.0-or-later
    All contributors are recorded in relicensecheck.pl

--- tests/testldapclient.cpp
+++ tests/testldapclient.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the KDE project
    SPDX-FileCopyrightText: 2005 David Faure <faure@kde.org>
 
-   SPDX-License-Identifier: LGPL-2.0-only
+   SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include "testldapclient.h"
