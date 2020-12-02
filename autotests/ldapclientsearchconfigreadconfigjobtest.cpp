c55b9f8dec1ccfb8c0499962d0dd1004ee49f751
Laurent Montel <montel@kde.org>
2020-12-02 08:52:14 +0100

    Start to implement autotest

---
+++ autotests/ldapclientsearchconfigreadconfigjobtest.cpp
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "ldapclientsearchconfigreadconfigjobtest.h"
+
+LdapClientSearchConfigReadConfigJobTest::LdapClientSearchConfigReadConfigJobTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
---
+++ autotests/ldapclientsearchconfigreadconfigjobtest.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#ifndef LDAPCLIENTSEARCHCONFIGREADCONFIGJOBTEST_H
+#define LDAPCLIENTSEARCHCONFIGREADCONFIGJOBTEST_H
+
+#include <QObject>
+
+class LdapClientSearchConfigReadConfigJobTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit LdapClientSearchConfigReadConfigJobTest(QObject *parent = nullptr);
+    ~LdapClientSearchConfigReadConfigJobTest() = default;
+};
+
+#endif // LDAPCLIENTSEARCHCONFIGREADCONFIGJOBTEST_H
