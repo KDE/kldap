8166bad5e5a84a0939b38ada5afd8f9cac0935b6
Laurent Montel <montel@kde.org>
2021-03-11 10:10:24 +0100

    This method seems to not exist in qt5.14 (we depend against 5.14)
    CCMAIL: tobydox@veyon.io

--- src/core/ldapdn.cpp
+++ src/core/ldapdn.cpp
@@ -46,10 +46,12 @@ bool LdapDN::LdapDNPrivate::isValidRDNString(const QString &rdn) const
         }
         return true;
     }
-
+#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
+    const QVector<QStringRef> components = rdn.splitRef(QLatin1Char('='));
+#else
     // Split the rdn into the attribute name and value parts
     const auto components = QStringView(rdn).split(QLatin1Char('='));
-
+#endif
     // We should have exactly two parts
     if (components.size() != 2) {
         return false;
