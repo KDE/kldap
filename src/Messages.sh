3c0c023c02d5dd6c5e669a274f53c3430dd79ed4
Alessandro Astone <ales.astone@gmail.com>
2023-12-20 16:30:21 +0100

    Fix the translation template version number (5->6)
    While this module does not need to be co-installable with its Qt5 version,
    change the version suffix for consistency.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
-$XGETTEXT core/*.cpp widgets/*.cpp -o $podir/libkldap5.pot
+$XGETTEXT core/*.cpp widgets/*.cpp -o $podir/libkldap6.pot
 
