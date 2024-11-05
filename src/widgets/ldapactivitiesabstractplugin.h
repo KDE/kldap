c5ba2ed423eb84cdaf1d0fd5409d7be81fdd8653
Laurent Montel <montel@kde.org>
2024-11-05 13:56:29 +0100

    Add ldapactivitiesabstractplugin

---
+++ src/widgets/ldapactivitiesabstractplugin.h
@@ -0,0 +1,27 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+#include "kldapwidgets_export.h"
+#include <QWidget>
+namespace KLDAPWidgets
+{
+class KLDAPWIDGETS_EXPORT LdapActivitiesAbstractPlugin : public QWidget
+{
+    Q_OBJECT
+public:
+    struct ActivitySettings {
+        QStringList activities;
+        bool enabled = false;
+    };
+
+    explicit LdapActivitiesAbstractPlugin(QWidget *parent = nullptr);
+    ~LdapActivitiesAbstractPlugin() override;
+
+    [[nodiscard]] virtual LdapActivitiesAbstractPlugin::ActivitySettings activitiesSettings() const = 0;
+    virtual void setActivitiesSettings(const LdapActivitiesAbstractPlugin::ActivitySettings &activitySettings) = 0;
+};
+}
