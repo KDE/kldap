345427fa14cb573882a308171366460d5a409dd2
Laurent Montel <montel@kde.org>
2026-01-01 11:35:11 +0100

    GIT_SILENT: happy new year

--- src/core/ldapsortproxymodel.h
+++ src/core/ldapsortproxymodel.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2024-2025 Laurent Montel <montel@kde.org>
+// SPDX-FileCopyrightText: 2024-2026 Laurent Montel <montel@kde.org>
 // SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 #pragma once
